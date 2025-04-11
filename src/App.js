c842d6dda72a2c504272c2d70e31ae41c2644b6b
denizmetn <dniizmetiiin@gmail.com>
2025-04-11 18:13:49 +0300

    a
    a

--- src/App.js
+++ src/App.js
@@ -4,7 +4,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      tuna bovfjvfkjvnfkfvnsdjkvnsvfvxvndklvndlkvndlkvnfdvlknvdlkrjkbzlak
+      tuna bovfjvfkjvnfkfvnsdjkvnsvfvxvndklvndlkvndlkvnfdvlknvdlkrjkbzlak  denniii
     </div>
   );
 }
